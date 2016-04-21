bc1093a6721ddaca9b5658ecc935dccb5374080e
OvyIstrate <ovidiu.istrate@techromix.com>
2016-04-21 17:50:14 +0300

    added jshint and installed bower + gulp

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
 
 var app = express();
+app.use(express.static('public'));
+app.use(express.static('src/views'));
 
 var port = 5000;
 
-app.listen(port, function(err){
-  console.log('listening on ' + port);
-})
+app.listen(port, function (err) {
+    console.log('listening on ' + port);
+});
