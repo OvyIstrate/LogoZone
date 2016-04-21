41c0c69299a5d88256eb0a61ae35134362295cc4
OvyIstrate <ovidiu.istrate@techromix.com>
2016-04-21 18:20:50 +0300

    configured wiredep for adding dependencies like a B0$$

--- gulpfile.js
+++ gulpfile.js
@@ -9,3 +9,15 @@ gulp.task('style', function () {
             verbose: true
         }));
 });
+gulp.task('inject', function () {
+    var wiredep = require('wiredep').stream;
+    var options = {
+        bowerJson: require('./bower.json'),
+        directory: './public/lib',
+        ignorePath: '../../public'
+    };
+
+    return gulp.src('./src/views/*.html')
+        .pipe(wiredep(options))
+        .pipe(gulp.dest('./src/views'));
+});
