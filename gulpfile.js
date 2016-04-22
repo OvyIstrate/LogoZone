913881945742a9db6b019faa4a3047fd0922172e
OvyIstrate <ovidiu.istrate@techromix.com>
2016-04-22 12:18:43 +0300

    configured gulp-inject and nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var nodemon = require('gulp-nodemon');
 var jsFiles = ['*.js', 'src/**/*.js'];
 
 gulp.task('style', function () {
@@ -11,6 +12,16 @@ gulp.task('style', function () {
 });
 gulp.task('inject', function () {
     var wiredep = require('wiredep').stream;
+    var inject = require('gulp-inject');
+
+    var injectSrc = gulp.src(['./src/css/*.css', './src/js/*.js'], {
+        read: false
+    });
+
+    var injectOptions = {
+        ignorePath: '/src'
+    };
+
     var options = {
         bowerJson: require('./bower.json'),
         directory: './public/lib',
@@ -19,5 +30,22 @@ gulp.task('inject', function () {
 
     return gulp.src('./src/views/*.html')
         .pipe(wiredep(options))
+        .pipe(inject(injectSrc, injectOptions))
         .pipe(gulp.dest('./src/views'));
+});
+
+gulp.task('serve', ['style', 'inject'],  function(){
+  var options = {
+    script: 'app.js',
+    delayTime: 1,
+    env: {
+      'PORT': 5000
+    },
+    watch: jsFiles,
+  }
+
+  return nodemon(options)
+      .on('restart', function(ev) {
+        console.log('Restarting...');
+      });
 });
