2ea716109f46889cf19ac62015820c335246c119
Ovidiu Istrate <ovidiu.istrate@techromix.com>
2016-08-10 17:18:53 +0300

    test commit

--- gulpfile.js
+++ gulpfile.js
@@ -40,7 +40,7 @@ gulp.task('serve', ['style', 'inject'],  function(){
     script: 'app.js',
     delayTime: 1,
     env: {
-      'PORT': 5000
+      'PORT': process.env.PORT || 5000
     },
     watch: jsFiles,
   };
