8e4a9b43f87dee68d01c47a7d615d098aa028de8
Ovidiu Istrate <ovidiu.istrate@techromix.com>
2016-08-10 17:02:41 +0300

    changed again

--- app.js
+++ app.js
@@ -72,6 +72,6 @@ app.get('*', function(req, res){
   res.render('index', {})
 })
 
-app.listen(config.port, function(err) {
+app.listen(config.port || 5000, function(err) {
     console.log('listening on ' + config.port);
 });
