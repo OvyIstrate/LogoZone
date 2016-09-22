e3f42d8713f2666eaaef4c1954128805938655fa
Ovidiu Istrate <ovidiu.istrate@techromix.com>
2016-09-22 18:20:10 +0300

    added new mongoose scheemas

---
+++ server/models/Spot.js
@@ -0,0 +1,69 @@
+var mongoose = require('mongoose'),
+    encryption = require('../utilities/encryption');
+
+var spotSchema = mongoose.Schema({
+    number: {type:Number, required: '{PATH} is required!', unique:true},
+    image: {type:Buffer},
+    contract: {type:contractSchema, unique:true},
+    type: typeSchema,
+    isAvailable:{type: Boolean, default:true},
+    salt: {type:String, required: '{PATH} is required!'},
+    hashed_pwd: {type:String, required: '{PATH} is required!'},
+    roles: [String]
+});
+
+userSchema.methods = {
+    authenticate: function(passwordToMatch) {
+        return encryption.hashPwd(this.salt, passwordToMatch) === this.hashed_pwd;
+    },
+    hasRole: function(role){
+      return this.roles.indexOf(role) > -1;
+    }
+}
+
+var User = mongoose.model('User', userSchema);
+
+function createDefaultUsers() {
+    User.find({}).exec(function(err, collection) {
+        if (collection.length === 0) {
+            var salt, hash;
+
+            salt = encryption.createSalt();
+            hash = encryption.hashPwd(salt, 'john');
+            User.create({
+                firstname: 'John',
+                lastname: 'Doe',
+                email: 'john.doe@tstmail.com',
+                username: 'john.doe',
+                salt: salt,
+                hashed_pwd: hash,
+                roles: ['admin']
+            });
+
+            salt = encryption.createSalt();
+            hash = encryption.hashPwd(salt, 'foo');
+
+            User.create({
+                firstname: 'Foo',
+                lastname: 'Bar',
+                email: 'foo.bar@tstmail.com',
+                username: 'foo.bar',
+                salt: salt,
+                hashed_pwd: hash,
+                roles: []
+            });
+
+            salt = encryption.createSalt();
+            hash = encryption.hashPwd(salt, 'jedi');
+            User.create({
+                firstname: 'Jedi',
+                lastname: 'Knight',
+                email: 'jedi.knight@tstmail.com',
+                username: 'jedi.knight',
+                salt: salt,
+                hashed_pwd: hash
+            });
+        }
+    });
+}
+exports.createDefaultUsers = createDefaultUsers;
---
+++ server/models/Type.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var typeSchema = new Schema({
+    description:{type:'String', required:'{PATH} is required'},
+    
+})
